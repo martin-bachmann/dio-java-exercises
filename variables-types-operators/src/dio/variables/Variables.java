9b2b2b39e835ff0285544a036ad00d3bda3b7608
Martin Bachmann <martinpezbach@gmail.com>
2022-11-04 14:00:54 -0300

    Variables exercises

---
+++ variables-types-operators/src/dio/variables/Variables.java
@@ -0,0 +1,27 @@
+package dio.variables;
+
+public class Variables {
+	public static void main(String[] args) {
+		int i;
+		
+		int nomeCorreto;
+		
+		final int NOME_CONSTANTE;
+		
+		i = 1;
+		
+		nomeCorreto = 2;
+		
+		NOME_CONSTANTE = 3;
+		
+		int outraVariavel = 4;
+		
+		final int OUTRA_CONSTANTE = 5;
+		
+		System.out.println(i);
+		System.out.println(nomeCorreto);
+		System.out.println(NOME_CONSTANTE);
+		System.out.println(outraVariavel);
+		System.out.println(OUTRA_CONSTANTE);
+	}
+}
